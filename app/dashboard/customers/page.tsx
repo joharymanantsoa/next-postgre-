0c3937bf048cc150293fca2d85ebfd85ed8679fc
ryimah <ryi.mantsoa@gmail.com>
2025-06-12 18:57:21 +0300

    rename: customer, invoices pages

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/app/ui/button"
+import Link from "next/link"
+
 export default function Page() {
-    return <h2>customers page</h2>;
+    return (
+        <div>
+            <h2>Customers</h2>
+            <Link href="\create" className="">
+            </Link>
+        </div>
+    )
+
 }
